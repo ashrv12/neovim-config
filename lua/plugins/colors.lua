4e19d00126379e92d9addafbbc4d3db4df6b3433
Shrv Bldna <151522619+ashrv12@users.noreply.github.com>
2026-01-28 21:19:27 +0800

    change the theme to cursor-dark

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -2,20 +2,37 @@ local function enable_transparency()
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
 return {
+    --    {
+    -- "folke/tokyonight.nvim",
+    -- config = function()
+    --     vim.cmd.colorscheme "tokyonight"
+    --     enable_transparency()
+    -- end
+    --    },
+    -- { "datsfilipe/vesper.nvim" },
     {
-	"folke/tokyonight.nvim",
-	config = function() 
-	    vim.cmd.colorscheme "tokyonight"
-	    enable_transparency()
-	end
+        "ydkulks/cursor-dark.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- vim.cmd.colorscheme("cursor-dark-midnight")
+            require("cursor-dark").setup({
+                -- For theme
+                style = "dark-midnight",
+                -- For a transparent background
+                transparent = true,
+                -- If you have dashboard-nvim plugin installed
+                dashboard = true,
+            })
+        end,
     },
     {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	},
-	opts = {
-	    theme = "tokyonight",
-	},
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            theme = "tokyonight",
+        },
     },
 }
